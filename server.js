84ca7ec176345719ce77b24f03e41719d4a30c60
gusttavo212 <gusttavo212@gmail.com>
2019-01-15 17:07:34 -0200

    Hello World route

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = new hapi.Server({
 });
 
 const start = async () => {
+    app.route({
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply) {
+            return `<h1>Hello Wolrd</h1>`         
+        }
+    });
+
     await app.start();
     console.log(`Server is running at ${app.info.port}`);    
 };
